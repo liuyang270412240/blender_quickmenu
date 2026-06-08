bf30e79cc2c508f9e345491ca04d722032c4706d
rog <270412240@qq.com>
2026-06-08 14:41:59 +0800

    2-1

--- __init__.py
+++ __init__.py
@@ -53,8 +53,8 @@ def draw_menu(self, items):
     elif 'nodetool' in item:
       icon = item['icon'] if 'icon' in item else 'NODETREE'
       if is_in_editmode():
-        operator = layout.operator('geometry.execute_node_group', text=title, icon=icon)
-        if operator:
+        if bpy.app.version < (5,1):
+          operator = layout.operator('geometry.execute_node_group', text=title, icon=icon)
           operator.name = item['nodetool']
           operator.asset_library_type = item['nodetool_library_type'] if 'nodetool_library_type' in item else 'CUSTOM'
           operator.asset_library_identifier = item['nodetool_library_identifier'] if 'nodetool_library_identifier' in item else 'QuickMenuLibrary'
